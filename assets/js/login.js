e472d66bb3a470a00dbe7492b11c6a26e5d9f859
Justice3984 <onyenmeliteobi@gmail.com>
2025-09-24 11:00:22 +0100

    Initial commit

--- assets/js/login.js
+++ assets/js/login.js
@@ -5,7 +5,7 @@ document.getElementById("loginForm").addEventListener("submit", async function(e
       const password = document.getElementById("password").value;
 
       try {
-        const response = await fetch("http://localhost:5000/api/users/login", {
+        const response = await fetch("https://cleaning-system-backend-3.onrender.com/api/users/login", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ email, password })
